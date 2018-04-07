bc432203fd33a22c43f4619b372a6f900bbd2f46
Anton Barty <barty@dhcp-visitor-enr-119-165.slac.stanford.edu>
2018-04-06 18:09:32 -0700

    Added a few more files needed for psana-conda

---
+++ source/pspython/jungfrau/src/jungfrau.py
@@ -0,0 +1,31 @@
+import numpy as np
+import psana
+
+class jungfrau (object) :
+
+    def __init__ ( self ) :
+        self.m_src = self.configSrc('source')
+        self.key_out = self.configStr('key_out')
+
+    def beginjob( self, evt, env ) :
+        # can use either the "full name" or the "alias" here
+        # type "detnames exp=xpptut15:run=410" to see the list of names
+        self.det = psana.Detector('Jungfrau1M',env)
+        pass
+ 
+    def beginrun( self, evt, env ) :
+        pass
+
+    def event( self, evt, env ) :
+        img = self.det.image(evt) #assembled
+        # or calib = self.det.calib(evt) for 3D unassembled
+        if img is None:
+            print 'jungfrau not found'
+            return
+        #print 'Found jungfrau with shape',img.shape,img
+        evt.put(img,self.m_src,self.key_out)
+        #for k in evt.keys():
+        #    if 'jungfrau' in str(k): print k
+
+    def endjob( self, evt, env ) :
+        pass
