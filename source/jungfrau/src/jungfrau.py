46be6e008e74b56b74ca9c82b91989f28291911e
Anton Barty <barty@dhcp-visitor-enr-119-165.slac.stanford.edu>
2018-04-07 20:09:36 -0700

    Added Python modules which pass data back to the event store

---
+++ source/jungfrau/src/jungfrau.py
@@ -0,0 +1,35 @@
+import numpy as np
+import psana
+
+class jungfrau (object) :
+
+    def __init__ ( self ) :
+        self.m_src = self.configSrc('source')
+        self.key_out = self.configStr('key_out')
+
+    def beginjob( self, evt, env ) :
+        # can use either the "full name" or the "alias" here
+        # type "detnames exp=xpptut15:run=410" to see the list of names
+	self.det = psana.Detector('MfxEndstation.0:Jungfrau.0',env)
+	#self.det = psana.Detector('Jungfrau1M',env)
+	#self.det = psana.Detector(self.m_src,env)
+        pass
+ 
+    def beginrun( self, evt, env ) :
+        pass
+
+    def event( self, evt, env ) :
+        #img = self.det.image(evt) #assembled
+        img = self.det.calib(evt) #for 3D unassembled
+	print 'Jungfrau image shape: ', img.shape
+
+        if img is None:
+            print 'jungfrau not found'
+            return
+        #print 'Found jungfrau with shape',img.shape,img
+        evt.put(img,self.m_src,self.key_out)
+        #for k in evt.keys():
+        #    if 'jungfrau' in str(k): print k
+
+    def endjob( self, evt, env ) :
+        pass
